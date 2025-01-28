53c16b961fb58366bfd5653221bfe9e1d00ab533
deni sugiarto <sugiarto.deni@gmail.com>
2025-01-28 20:27:32 +0700

    refactor(hero):loop the profile images and adjust first column position

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image';
+import DummyImage from '@/assets/images/dummy-profile.jpeg';
+import ImageSlide from '@/assets/images/image-slide.png';
+import MarkIcon from '@/assets/images/mark.svg';
 import StarIcon from '@/assets/images/star.svg';
 import { Button } from '@/components/ui/button';
-import ImageSlide from '@/assets/images/image-slide.png';
-import Deni from '@/assets/images/deni.jpeg';
-import MarkIcon from '@/assets/images/mark.svg';
+import Image from 'next/image';
 export default function Hero() {
     return (
         <section className='grid grid-cols-1 md:grid-cols-2 gap-x-20 before:left-0 before:top-0 before:w-full before:h-full before:absolute before:opacity-80 before:bg-hero-pattern md:pt-12 before:-z-10 md:pb-16 px-5 xl:px-0'>
-            <div className='max-w-[560px]'>
+            <div className='max-w-[560px] flex flex-col items-start justify-center'>
                 <div className='flex py-2 border rounded-full px-4 gap-x-2 text-sm leading-none items-center max-w-max'>
                     <Image src={StarIcon} height={16} width={16} alt='star icon' />
                     <p>Komunitas IT di Blitar</p>
@@ -49,34 +49,18 @@ export default function Hero() {
                 </div>
                 <div className='bg-card rounded-2xl p-6'>
                     <div className='flex mb-9'>
-                        <Image
-                            src={Deni}
-                            alt='deni profile'
-                            width={40}
-                            height={40}
-                            className='rounded-full object-cover'
-                        />
-                        <Image
-                            src={Deni}
-                            alt='deni profile'
-                            width={40}
-                            height={40}
-                            className='rounded-full object-cover -ml-3'
-                        />
-                        <Image
-                            src={Deni}
-                            alt='deni profile'
-                            width={40}
-                            height={40}
-                            className='rounded-full object-cover -ml-3'
-                        />
-                        <Image
-                            src={Deni}
-                            alt='deni profile'
-                            width={40}
-                            height={40}
-                            className='rounded-full object-cover -ml-3'
-                        />
+                        {Array(4)
+                            .fill('')
+                            .map((_item, index) => (
+                                <Image
+                                    key={'profile-' + index}
+                                    src={DummyImage}
+                                    alt='image profile'
+                                    width={40}
+                                    height={40}
+                                    className={`rounded-full object-cover ${index !== 0 ? '-ml-3' : ''}`}
+                                />
+                            ))}
                     </div>
                     <p className='font-bold text-2xl mb-3'>60+</p>
                     <p className='text-secondary'>Active Member</p>
