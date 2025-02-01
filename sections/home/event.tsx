dcda98a7a260b82cd786250b34e7bc0b250345ee
deni sugiarto <sugiarto.deni@gmail.com>
2025-02-01 15:29:06 +0700

    feat(event): add image

--- sections/home/event.tsx
+++ sections/home/event.tsx
@@ -1,10 +1,11 @@
 import CardLabel from '@/components/ui/card-label';
 import EventAccordion from './event-accordion';
-
+import Image from 'next/image';
+import ActivityImage from '@/assets/images/activity.jpg';
 function Event() {
     return (
-        <section className='grid animate-fadeLeft grid-cols-1 gap-x-20 gap-y-6 px-5 pb-16 pt-12 lg:grid-cols-2 xl:px-0'>
-            <div>
+        <section className='grid grid-cols-1 gap-x-20 gap-y-6 px-5 pb-16 pt-12 lg:grid-cols-2 xl:px-0'>
+            <div className='animate-fadeLeft'>
                 <CardLabel label='Aktivitas' />
                 <h2 className='mt-6 text-2xl font-medium xl:text-4xl'>
                     Mewadahi kamu melalui berbagai kegiatan
@@ -14,7 +15,15 @@ function Event() {
                 </p>
                 <EventAccordion className='mt-6' />
             </div>
-            <div></div>
+            <div className='ml-auto animate-fadeRight'>
+                <Image
+                    src={ActivityImage}
+                    className='rounded-2xl'
+                    width={600}
+                    height={375}
+                    alt='Event community'
+                />
+            </div>
         </section>
     );
 }
