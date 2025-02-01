3a4f569a620995dd32eb2a2ff0890b1cbbb14da5
deni sugiarto <sugiarto.deni@gmail.com>
2025-02-01 15:08:11 +0700

    feat(header): create mobile navigation menu

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,9 @@
 import logo from '@/assets/images/patria dev logo only.png';
+import { MenuIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from './ui/button';
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from './ui/drawer';
 
 type link = {
     title: string;
@@ -29,7 +31,7 @@ export const navigationLink: link[] = [
 export default function Header() {
     return (
         <header className='border-b'>
-            <div className='container flex h-[72px] items-center justify-between px-5 md:mx-auto xl:px-0'>
+            <div className='container mx-auto flex h-[72px] items-center justify-between px-5 xl:px-0'>
                 <Link href='/'>
                     <Image src={logo} alt='patria dev logo' width={40} height={40} />
                 </Link>
@@ -42,7 +44,26 @@ export default function Header() {
                             </Link>
                         ))}
                 </div>
-                <Button rounded='pill'>Hubungi Kami</Button>
+                <Button className='hidden md:block' rounded='pill'>
+                    Hubungi Kami
+                </Button>
+                {/** Mobile Menu */}
+                <Drawer>
+                    <DrawerTrigger className='md:hidden'>
+                        <MenuIcon />
+                    </DrawerTrigger>
+                    <DrawerContent>
+                        <DrawerHeader>
+                            <div className='flex flex-col gap-8'>
+                                {navigationLink.map(({ title, url }) => (
+                                    <Link key={title} href={url} className='block capitalize'>
+                                        <DrawerTitle>{title}</DrawerTitle>
+                                    </Link>
+                                ))}
+                            </div>
+                        </DrawerHeader>
+                    </DrawerContent>
+                </Drawer>
             </div>
         </header>
     );
